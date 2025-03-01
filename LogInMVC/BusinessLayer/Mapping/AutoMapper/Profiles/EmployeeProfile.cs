a9f27dd084b68ad96c41127b4e63ced73987560b
emre-aksu <emreaksu92@outlook.com>
2025-03-01 22:57:35 +0300

    EmployeeProfile added to the Business layer for AutoMapper.

---
+++ LogInMVC/BusinessLayer/Mapping/AutoMapper/Profiles/EmployeeProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ModelLayer.Dtos;
+using ModelLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Mapping.AutoMapper.Profiles
+{
+   public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<EmployeeAddDto, Employee>();
+            CreateMap<EmployeeEditDto, Employee>();
+        }
+    }
+}
